7b92b178eeb593535d8ddf66baf0207a06ce6b88
haruki7049 <tontonkirikiri@gmail.com>
2025-08-26 16:53:21 +0900

    UpdatE

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::prelude::*;
+use clap::Parser;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _args: CLIArgs = CLIArgs::parse();
+
+    App::new().add_plugins(MinimalPlugins).run();
+
+    Ok(())
 }
+
+#[derive(Debug, Parser)]
+#[clap(about, version, author)]
+struct CLIArgs {}
